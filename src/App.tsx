fe6978dac50b2029e1358d18853ff90eb9f20e75
bryllemutia <bryllemutia3@gmail.com>
2024-06-13 23:44:12 +0800

    fix: overlap between drawer and appbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,34 @@ import React, { useState } from "react";
 import "./App.css";
 import CustomAppBar from "./components/CustomAppBar";
 import CustomDrawer from "./components/CustomDrawer";
+import { Box } from "@mui/material";
 
 function App() {
-   const [isNavOpen, setIsNavToggle] = useState(false);
+   const [isNavOpen, setIsNavToggle] = useState(true);
 
    const handleNavToggle = () => {
       setIsNavToggle((prev) => !prev);
    };
 
    return (
-      <>
-         <CustomAppBar handleNavToggle={handleNavToggle} />
+      <Box
+         sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            flexShrink: "1",
+            width: "100%",
+         }}
+      >
          <CustomDrawer
             handleNavToggle={handleNavToggle}
             isNavOpen={isNavOpen}
          />
-      </>
+         <CustomAppBar handleNavToggle={handleNavToggle} />
+      </Box>
    );
 }
 
 export default App;
+
+// TODO: Implement react-router
+// TODO: Add auth pages using material UI
