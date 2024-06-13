57871c187fe034b1ef45c6b74d209eaf80bc1e60
bryllemutia <bryllemutia3@gmail.com>
2024-06-13 23:43:09 +0800

    feat: custom app bar

--- src/components/CustomAppBar.tsx
+++ src/components/CustomAppBar.tsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-   AppBar,
-   Box,
-   CssBaseline,
-   IconButton,
-   Toolbar,
-   Typography,
-} from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
@@ -29,57 +22,54 @@ export default function CustomAppBar({ handleNavToggle }: CustomAppBarProps) {
    };
 
    return (
-      <Box sx={{ display: "flex" }}>
-         <CssBaseline />
-         <AppBar position="static">
-            <Toolbar>
-               <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
-                  sx={{ mr: 2 }}
-                  onClick={handleNavToggle}
-               >
-                  <MenuIcon />
-               </IconButton>
-               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  Code Review Tool
-               </Typography>
-               {auth && (
-                  <div>
-                     <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleMenu}
-                        color="inherit"
-                     >
-                        <AccountCircle />
-                     </IconButton>
-                     <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                           vertical: "top",
-                           horizontal: "right",
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                           vertical: "top",
-                           horizontal: "right",
-                        }}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                     >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                     </Menu>
-                  </div>
-               )}
-            </Toolbar>
-         </AppBar>
-      </Box>
+      <AppBar position="static" sx={{ width: "auto", flexGrow: "1" }}>
+         <Toolbar>
+            <IconButton
+               size="large"
+               edge="start"
+               color="inherit"
+               aria-label="menu"
+               sx={{ mr: 2 }}
+               onClick={handleNavToggle}
+            >
+               <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+               Code Review Tool
+            </Typography>
+            {auth && (
+               <div>
+                  <IconButton
+                     size="large"
+                     aria-label="account of current user"
+                     aria-controls="menu-appbar"
+                     aria-haspopup="true"
+                     onClick={handleMenu}
+                     color="inherit"
+                  >
+                     <AccountCircle />
+                  </IconButton>
+                  <Menu
+                     id="menu-appbar"
+                     anchorEl={anchorEl}
+                     anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                     }}
+                     keepMounted
+                     transformOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                     }}
+                     open={Boolean(anchorEl)}
+                     onClose={handleClose}
+                  >
+                     <MenuItem onClick={handleClose}>Profile</MenuItem>
+                     <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </Menu>
+               </div>
+            )}
+         </Toolbar>
+      </AppBar>
    );
 }
