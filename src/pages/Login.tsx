f993dbbb95849ca96ffc6860e28ede56b5584338
bryllemutia <bryllemutia3@gmail.com>
2024-06-15 00:57:53 +0800

    added auth pages signup / login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,150 @@
+import React, { useContext, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Copyright from "../components/Copyright";
+import { AppContext } from "../context/AppContext";
+import { AppContextType } from "../context/types";
+import { useNavigate, Link as RouterLink } from "react-router-dom";
+import AuthService from "../services/AuthService";
+
+export default function LogIn() {
+   const [email, setEmail] = useState<string | null>(null);
+   const [password, setPassword] = useState<string | null>(null);
+   const [rememberMe, setRememberMe] = useState(false);
+
+   const { handleAuthChange, handleUserDetailsChange } = useContext(
+      AppContext
+   ) as AppContextType;
+
+   const navigate = useNavigate();
+
+   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(e.target.value);
+   };
+   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+   };
+   const handleRememberMe = () => {
+      setRememberMe((prev) => !prev);
+   };
+
+   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      if (email && password) {
+         AuthService.loginUser(email, password, rememberMe).then((response) => {
+            console.log(response.data);
+
+            const { id, name, email, access_token } = response.data;
+            handleUserDetailsChange({ id, name, email });
+            handleAuthChange(true);
+
+            // set token to localstorage
+            localStorage.setItem("access_token", access_token);
+
+            // redirect to homepage
+            navigate("/");
+         });
+      }
+   };
+
+   return (
+      <Container
+         component="main"
+         maxWidth="xs"
+         sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+         }}
+      >
+         <CssBaseline />
+         <Box
+            sx={{
+               marginTop: 8,
+               display: "flex",
+               flexDirection: "column",
+               alignItems: "center",
+            }}
+         >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+               <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+               Sign in
+            </Typography>
+            <Box
+               component="form"
+               onSubmit={handleSubmit}
+               noValidate
+               sx={{ mt: 1 }}
+            >
+               <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={handleEmailChange}
+               />
+               <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={handlePasswordChange}
+               />
+               <FormControlLabel
+                  control={
+                     <Checkbox
+                        value="remember"
+                        color="primary"
+                        onChange={handleRememberMe}
+                     />
+                  }
+                  label="Remember me"
+               />
+               <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+               >
+                  Sign In
+               </Button>
+               <Grid container>
+                  <Grid item xs>
+                     <Link variant="body2">Forgot password?</Link>
+                  </Grid>
+                  <Grid item>
+                     <RouterLink to="/auth/signup">
+                        <Link component="p" variant="body2">
+                           {"Don't have an account? Sign Up"}
+                        </Link>
+                     </RouterLink>
+                  </Grid>
+               </Grid>
+            </Box>
+         </Box>
+         <Copyright sx={{ mt: 10, mb: 4 }} />
+      </Container>
+   );
+}
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,151 @@
+import React, { useContext, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import AuthService from "../services/AuthService";
+
+import Copyright from "../components/Copyright";
+import { AppContextType } from "../context/types";
+import { AppContext } from "../context/AppContext";
+import { useNavigate, Link as RouterLink } from "react-router-dom";
+
+export default function SignUp() {
+   const [username, setUsername] = useState<string | null>(null);
+   const [email, setEmail] = useState<string | null>(null);
+   const [password, setPassword] = useState<string | null>(null);
+
+   const { handleAuthChange, handleUserDetailsChange } = useContext(
+      AppContext
+   ) as AppContextType;
+
+   const navigate = useNavigate();
+
+   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUsername(e.target.value);
+   };
+   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(e.target.value);
+   };
+   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+   };
+
+   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      if (email && password && username) {
+         AuthService.signupUser(email, username, password).then((response) => {
+            console.log(response.data);
+
+            const { id, name, email, access_token } = response.data;
+            handleUserDetailsChange({ id, name, email });
+            handleAuthChange(true);
+
+            // set token to localstorage
+            localStorage.setItem("access_token", access_token);
+
+            // redirect to homepage
+            navigate("/");
+         });
+      }
+   };
+
+   return (
+      <Container
+         component="main"
+         maxWidth="xs"
+         sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+         }}
+      >
+         <CssBaseline />
+         <Box
+            sx={{
+               marginTop: 8,
+               display: "flex",
+               flexDirection: "column",
+               alignItems: "center",
+            }}
+         >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+               <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+               Sign up
+            </Typography>
+            <Box
+               component="form"
+               noValidate
+               onSubmit={handleSubmit}
+               sx={{ mt: 3 }}
+            >
+               <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                     <TextField
+                        autoComplete="username"
+                        name="username"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        autoFocus
+                        onChange={handleNameChange}
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        onChange={handleEmailChange}
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        onChange={handlePasswordChange}
+                     />
+                  </Grid>
+               </Grid>
+               <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+               >
+                  Sign Up
+               </Button>
+               <Grid container justifyContent="flex-end">
+                  <Grid item>
+                     <RouterLink to="/auth">
+                        <Link component="p" variant="body2">
+                           Already have an account? Sign in
+                        </Link>
+                     </RouterLink>
+                  </Grid>
+               </Grid>
+            </Box>
+         </Box>
+         <Copyright sx={{ mt: 10 }} />
+      </Container>
+   );
+}
