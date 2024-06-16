8a8cc845f2af84356d19b7cc86dc6248b7f829c7
bryllemutia <bryllemutia3@gmail.com>
2024-06-16 20:58:38 +0800

    added input and new review dialog to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,40 @@
+import { Box } from "@mui/material";
+import ChatInput from "../components/ChatInput";
+import PackageSelectDialog from "../components/PackageSelectDialog";
+
 export default function Home() {
    return (
-      <div>
-         <div>Homepage</div>
-      </div>
+      <Box
+         sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+         }}
+      >
+         <Box
+            sx={{
+               width: "100%",
+               display: "flex",
+               justifyContent: "center",
+            }}
+         >
+            <PackageSelectDialog />
+         </Box>
+
+         <Box
+            sx={{
+               position: "absolute",
+               bottom: "0px",
+               left: "50%",
+               transform: "translateX(-25%)",
+               marginBottom: "1em",
+            }}
+         >
+            <Box sx={{ width: "40em" }}>
+               <ChatInput />
+            </Box>
+         </Box>
+      </Box>
    );
 }
