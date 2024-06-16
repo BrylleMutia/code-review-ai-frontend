9b860c2b95caf9b39c94e5b4f0a6c4d47a95a015
bryllemutia <bryllemutia3@gmail.com>
2024-06-16 20:56:09 +0800

    added types for services

--- src/services/types.ts
+++ src/services/types.ts
@@ -5,5 +5,27 @@ export type BaseUserDetails = {
 };
 
 export type UserDetailsAuth = {
-   access_token: string
-} & BaseUserDetails
+   access_token: string;
+} & BaseUserDetails;
+
+export type FindPackageRes = {
+   packages: string[];
+};
+
+export type ModuleDetails = {
+   referenced_type: string;
+   referenced_name: string;
+};
+
+export type SetPackageRes = {
+   data: {
+      line_count: number;
+      package_name: string;
+      ref_modules: ModuleDetails[];
+      ref_tables: string[];
+   };
+};
+
+export type PromptRes = {
+   response: string;
+};
