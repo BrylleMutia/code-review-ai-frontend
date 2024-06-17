22e1bcce82699bd1d93fbeb45384ca9adb5fb67d
bryllemutia <bryllemutia3@gmail.com>
2024-06-17 20:04:09 +0800

    initialized MUI theme on app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
 
 import App from "./App.tsx";
 import AppContextProvider from "./context/AppContext.js";
+import theme from "./theme/index.ts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -13,9 +15,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <Route
                path="/*"
                element={
-                  <AppContextProvider>
-                     <App />
-                  </AppContextProvider>
+                  <ThemeProvider theme={theme}>
+                     <AppContextProvider>
+                        <App />
+                     </AppContextProvider>
+                  </ThemeProvider>
                }
             />
          </Routes>
