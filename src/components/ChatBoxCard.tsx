a9f15de4599e97730ce27d249402e61b345508fc
bryllemutia <bryllemutia3@gmail.com>
2024-06-17 20:07:41 +0800

    feat: chatbox card component

---
+++ src/components/ChatBoxCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+
+type ChatBoxCardProps = {
+   children: React.ReactNode;
+};
+
+export default function ChatBoxCard({ children }: ChatBoxCardProps) {
+   return (
+      <Box sx={{ minWidth: "500px" }}>
+         <Card variant="outlined">{children}</Card>
+      </Box>
+   );
+}
