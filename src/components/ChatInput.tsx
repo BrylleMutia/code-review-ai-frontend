6dfe669d625d1370031df4377dc70ff8be2353e3
bryllemutia <bryllemutia3@gmail.com>
2024-06-23 16:11:48 +0800

    feat: custom chat input

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -7,11 +7,18 @@ import SendIcon from "@mui/icons-material/Send";
 import ReviewService from "../services/ReviewService";
 import { AppContextType } from "../context/types";
 import { AppContext } from "../context/AppContext";
+import { errorHandler } from "../utils/error";
 
 export default function ChatInput() {
-   const [prompt, setPrompt] = useState<string | null>(null);
+   const [prompt, setPrompt] = useState<string>("");
 
-   const { isSyncLoading } = useContext(AppContext) as AppContextType;
+   const {
+      isSyncLoading,
+      handleSetSyncLoading,
+      handleUpdatePrompts,
+      promptResponses,
+      handleChangeSnackbar,
+   } = useContext(AppContext) as AppContextType;
 
    const handlePromptChange = (
       event: React.ChangeEvent<HTMLTextAreaElement>
@@ -22,10 +29,34 @@ export default function ChatInput() {
    const handleSubmitPrompt = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
 
-      if (prompt) {
-         ReviewService.sendPrompt(prompt).then((response) => {
-            console.log(response.data);
+      if (promptResponses && prompt) {
+         handleSetSyncLoading(true);
+
+         // set empty placeholder prompt for loader while waiting for response
+         // prompt id is last prompt + 1
+         handleUpdatePrompts({
+            id: promptResponses.length + 1,
+            prompt: "",
+            response: "",
+            isLoading: true,
          });
+
+         const newPrompt = prompt;
+         setPrompt("");
+
+         ReviewService.sendPrompt(newPrompt)
+            .then((response) => {
+               // set true prompt details to replace placeholder prompt
+               handleUpdatePrompts({
+                  id: promptResponses.length + 1,
+                  prompt,
+                  response: response.data.response,
+                  isLoading: false,
+               });
+
+               handleSetSyncLoading(false);
+            })
+            .catch((err) => errorHandler(err, handleChangeSnackbar));
       }
    };
 
@@ -37,10 +68,11 @@ export default function ChatInput() {
       >
          <TextField
             required
+            value={prompt}
             id="prompt"
             name="prompt"
             autoComplete="prompt"
-            placeholder="What can I help you with?"
+            placeholder="Type something"
             autoFocus
             onChange={handlePromptChange}
             fullWidth
